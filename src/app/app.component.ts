31b3940546f9a2ea4af9f16e0b282eebbeb56e19
sangay-yonten <sangay9yonten@gmail.com>
2023-03-14 14:59:51 +0600

    feat: dark mode and refactor code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {BehaviorSubject} from 'rxjs';
 
 @Component({
   selector: 'arm-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
 
+export class AppComponent implements OnInit {
+  showMenu = false;
+  isDarkEnable = false;
+  presentTheme$ = new BehaviorSubject<string>('theme-light');
+  ngOnInit() {
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme) {
+      this.presentTheme$.next(savedTheme);
+    }
+  }
+  changeTheme() {
+    this.presentTheme$.value === 'theme-light'
+      ? this.presentTheme$.next('theme-dark')
+      : this.presentTheme$.next('theme-light');
+    localStorage.setItem('theme', this.presentTheme$.value);
+    this.isDarkEnable = !this.isDarkEnable;
+  }
+
+  toggleNavbar(){
+    this.showMenu = !this.showMenu;
+  }
 }
